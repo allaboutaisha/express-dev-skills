c1f7d2b0dc5a332310e297dc74f8e23147da2e1a
Aisha Mamoor <aisha.mamoor@gmail.com>
2022-12-12 17:16:36 -0500

    added skills to models

---
+++ models/skills.js
@@ -0,0 +1,7 @@
+const mySkills = [
+    {skill: HTML learned: true}
+    {skill: Javascript learned: true}
+    {skill: JQuery learned: true}
+    {skill: CSS learned: true}
+    {skill: Express learned: false}
+]
